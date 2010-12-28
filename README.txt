63a24d18fc6de66b0bdbcdb8a21adc662627911c
Jonas Baumann <jone@jone.ch>
2010-12-28 22:43:22 +0100

    created package with paster

---
+++ README.txt
@@ -0,0 +1,7 @@
+Introduction
+============
+
+The zope3 component registry inspector is used for inspecting adapters
+and utilities registered in the z3 component registry. It provides a view
+registred on the app level for searching and inspecting the registry. In
+addition it also provides a json API.
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.z3cinspector',
+      version=version,
+      description="Zope3 component registry inspector",
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='zope3 component registry inspector',
+      author='Jonas Baumann, 4teamwork.ch',
+      author_email='mailto:info@4teamwork.ch',
+      url='http://github.com/collective/collective.z3cinspector',
+      license='GPL2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+        ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
