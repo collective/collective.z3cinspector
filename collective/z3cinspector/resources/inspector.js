ac92434408b9bbf0868582a98ca059299e19d74e
Jonas Baumann <jone@jone.ch>
2010-12-30 15:55:20 +0100

    some layouting: tabs, css

--- collective/z3cinspector/resources/inspector.js
+++ collective/z3cinspector/resources/inspector.js
@@ -1,15 +1,19 @@
 $(function() {
 
+  /* TABS */
+
+  $('ul.tabs').tabs('ul.panes > li');
+
   /* UTILITIES */
 
   $("#utilityInterface").autocomplete(
     '/@@inspector-search-utility/search_interface', {
-      width: 400
+      width: 500
     });
 
   var ac_utility_name = $('#utilityName').autocomplete(
     '/@@inspector-search-utility/search_name', {
-      width: 400,
+      width: 500,
       extraParams: {
         iface: function() {
           return $("#utilityInterface").val();
