cf5384b42237cf6e91db0d6a7f1d01704f9ae0b3
Jonas Baumann <jone@jone.ch>
2010-12-31 16:32:10 +0100

    renamed README to .rst

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='collective.z3cinspector',
       version=version,
       description="Zope3 component registry inspector",
-      long_description=open("README.txt").read() + "\n" + \
+      long_description=open("README.rst").read() + "\n" + \
           open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
